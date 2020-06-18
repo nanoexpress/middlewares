ebd0b9117b216b63e9c86159fb61efc9ce0ce0cd
dalisoft <dalisoft@mail.ru>
2020-06-18 17:10:31 +0500

    chore(pkg): make universal exports

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+const external = ['querystring', 'graphql', 'fs', 'path'];
+
+export default ['body-parser', 'static', 'graphql', 'redoc'].map((name) => ({
+  input: `./${name}/${name}.es.js`,
+  output: {
+    format: 'cjs',
+    file: `./${name}/cjs/${name}.cjs.js`,
+    strict: false
+  },
+  external
+}));
