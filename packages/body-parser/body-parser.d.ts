d297a96ddff1b271cde541e05a55620ee681418b
dalisoft <dalisoft@mail.ru>
2021-05-23 17:38:03 +0500

    fix(@nanoexpress/middleware-body-parser): type declaration fix

--- packages/body-parser/body-parser.d.ts
+++ packages/body-parser/body-parser.d.ts
@@ -1,8 +1,12 @@
+import { RequestListener } from 'http';
+
 interface IBodyParserOptions {
   json: boolean;
   urlEncoded: boolean;
 }
 
-declare function bodyParser<T>(options?: IBodyParserOptions): T | Promise<T>;
+declare function bodyParser(
+  options?: IBodyParserOptions
+): Promise<RequestListener>;
 
 export = bodyParser;
